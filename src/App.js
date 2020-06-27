352c9d4aa11538cfc2c10afc786950b2dc747874
enveal <envealsood@gmail.com>
2020-06-28 03:59:24 +0530

    Colors corrected

--- src/App.js
+++ src/App.js
@@ -24,9 +24,9 @@ const App = () => {
                   label:'People',
                   data:rawd,
                   backgroundColor:[
-                    'rgba(255, 99, 132, 0.6)',
-                    'rgba(54, 162, 235, 0.6)',
-                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(255, 51, 0, 0.6)',
+                    'rgba(0, 153, 51, 0.6)',
+                    'rgba(204, 0, 0,0.6)',
                   ]
                 }
               ]
