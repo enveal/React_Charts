e917f91b383fe2c35fd78037c973c9f85cb74899
enveal <envealsood@gmail.com>
2020-06-28 03:50:21 +0530

    Updates

---
+++ precache-manifest.5f8089238cba6a10e9c0f834a5dc731f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "091b2c7ba3b3324e6826ff0532fb8779",
+    "url": "/React_Charts/index.html"
+  },
+  {
+    "revision": "1b48a3604b61da6ed2c0",
+    "url": "/React_Charts/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4fd3f5dd26971ae5d788",
+    "url": "/React_Charts/static/js/2.3c5d01df.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/React_Charts/static/js/2.3c5d01df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b48a3604b61da6ed2c0",
+    "url": "/React_Charts/static/js/main.20588310.chunk.js"
+  },
+  {
+    "revision": "7405103c7f826a486123",
+    "url": "/React_Charts/static/js/runtime-main.bb3c48ff.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React_Charts/precache-manifest.5f8089238cba6a10e9c0f834a5dc731f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_Charts/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
