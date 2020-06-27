a4fd5a12fbd88bfe19e64466a20f0f5eaf633005
enveal <envealsood@gmail.com>
2020-06-28 03:39:43 +0530

    Files to be Added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Pie, Bar ,Line} from "react-chartjs-2";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [chartData, setChartData] = useState({});
+ // const [employeeSalary, setEmployeeSalary] = useState([]);
+  //const [employeeAge, setEmployeeAge] = useState([]);
+
+  const chart =()=>{
+    let rawd=[];
+    let data={};
+    axios
+    .get("https://coronavirus-19-api.herokuapp.com/countries/INDIA")
+    .then(res =>{
+        console.log(res.data);
+        data=res.data;
+        console.log(data);
+        rawd=[res.data.active,res.data.recovered,res.data.deaths]
+        setChartData({
+            labels: ['Active Cases','Recoverd cases','Total Deaths'],
+            datasets:[
+                {
+                  label:'People',
+                  data:rawd,
+                  backgroundColor:[
+                    'rgba(255, 99, 132, 0.6)',
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                  ]
+                }
+              ]
+        });
+    })
+    .catch(err =>{
+        console.log(err);
+    });
+    console.log(rawd)
 }
-
+useEffect(()=>{
+    chart();
+},[]);
+return(
+    <div className="App">
+     <h1>Corona Cases In India</h1>
+        <div>
+        <Pie
+          data={chartData}
+          options={{
+            title:{
+              //display:this.props.displayTitle,
+              text:'CORONA CASES IN India',
+              fontSize:25
+            },
+            legend:{
+              display:true,
+              position:"right"
+            }
+          }}
+        />  
+        <br/>
+        <Bar
+          data={chartData}
+          options={{
+            title:{
+              //display:this.props.displayTitle,
+              text:'CORONA CASES IN India',
+              fontSize:25
+            },
+            legend:{
+              display:true,
+              position:"right"
+            }
+          }}
+        />  
+        <br />
+        <Line
+          data={chartData}
+          options={{
+            title:{
+              //display:this.props.displayTitle,
+              text:'CORONA CASES IN India',
+              fontSize:25
+            },
+            legend:{
+              display:true,
+              position:"right"
+            }
+          }}
+        />  
+        </div>   
+    </div>
+)
+        }
 export default App;
